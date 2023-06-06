4e9a78ee802e080fed473eb0826863ce1f182490
Pedro Jeacomo <schoolpepoo@gmail.com>
2023-06-06 12:29:25 -0300

    Início controle de versão

---
+++ src/main/java/com/itb/inf2fm/comercio/model/Produto.java
@@ -0,0 +1,15 @@
+package com.itb.inf2fm.comercio.model;
+
+public class Produto {
+	
+	private Long id;
+	private String nome;
+	private String descricao;
+	private String codigoBarras;
+	private double preco;
+	
+	//public: Acesso liberado para todas as classes
+	//private: Acesso permitido apenas para os membros da própria classe.
+	//Entende-se por membros(atributos e ou métodos)
+	
+}

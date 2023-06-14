1e565c080f3ace5735281af4dd1a9d5d9160ac97
Pedro Jeacomo <schoolpepoo@gmail.com>
2023-06-14 08:18:28 -0300

    Edição da LojaController

--- src/main/java/com/itb/inf2fm/comercio/controller/LojaController.java
+++ src/main/java/com/itb/inf2fm/comercio/controller/LojaController.java
@@ -1,5 +1,8 @@
 package com.itb.inf2fm.comercio.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,11 +14,28 @@ import com.itb.inf2fm.comercio.model.Produto;
 
 public class LojaController {
 	
+	List<Produto> listaDeProdutos = new ArrayList<Produto>();
+	
 	@GetMapping("/listar")
 	public String listarProdutos() {
 		
 		Produto p1 = new Produto();
-		p1.id = 20l;
+		p1.setId(20l);
+		p1.setNome("Máquina de Lavar Brastemp 15l");
+		p1.setDescricao("Produto Linha Branca com painel digital");
+		p1.setCodigoBarras("TV4785667GHTN778");
+		p1.setPreco(3215.89);
+		
+		Produto p2 = new Produto();
+		p2.setId(20l);
+		p2.setNome("Televisor 70'");
+		p2.setDescricao("56FUH64378FFG9");
+		p2.setCodigoBarras("Televisor Tela Plana led Samsung");
+		p2.setPreco(6326.12);
+		
+		//Adicionando os produtos à lista
+		listaDeProdutos.add(p1);
+		listaDeProdutos.add(p2);
 		
 		return "produtos";
 	
